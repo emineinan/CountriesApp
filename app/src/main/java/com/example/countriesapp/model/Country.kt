0998c1faac56b3625fd0803b904d9b2270c6b73f
Emine İnan <emineinanx@gmail.com>
2021-08-11 17:33:44 +0300

    Create model class

---
+++ app/src/main/java/com/example/countriesapp/model/Country.kt
@@ -0,0 +1,10 @@
+package com.example.countriesapp.model
+
+data class Country(
+    val countryName: String?,
+    val countryCapital: String?,
+    val countryRegion: String?,
+    val countryCurrency: String?,
+    val countryLanguage: String?,
+    val imageUrl: String?
+)
